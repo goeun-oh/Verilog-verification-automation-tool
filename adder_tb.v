68ac4188ecc4b567d48c2bf863ef4b073dac33e2
goeun-oh <103434703+goeun-oh@users.noreply.github.com>
2025-02-20 20:31:14 +0900

    write 되는 형식 수정

--- adder_tb.v
+++ adder_tb.v
@@ -11,20 +11,19 @@ module adder_tb;
 
     initial begin
         fd_in = $fopen("input.txt", "r"); // 입력 파일 열기
-        fd_out = $fopen("verilog_output.txt", "w"); // 출력 파일 열기
+        fd_out = $fopen("output.txt", "w"); // 출력 파일 열기
 
         if (fd_in == 0) begin
             $display("Error: input.txt file not found.");
             $finish;
         end
 
-        $fwrite(fd_out, "A B Cin | Sum Cout\n"); // 결과 헤더 작성
-
         while (!$feof(fd_in)) begin
             scan_result = $fscanf(fd_in, "%b %b %b\n", a, b, cin); // input.txt에서 데이터 읽기
             #10; // 연산 대기
 
-            $fwrite(fd_out, "%b %b %b | %b %b\n", a, b, cin, sum, cout); // 결과 저장
+            // 새로운 출력 형식 적용
+            $fwrite(fd_out, "%b %b %b -> sum: %b, cout: %b\n", a, b, cin, sum, cout);
         end
 
         $fclose(fd_in);
