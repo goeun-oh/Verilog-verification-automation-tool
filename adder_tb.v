772952a5aea9f114518a917a9e312045f7ab3bd4
jhyang1203 <xhvldk3@gmail.com>
2025-02-20 15:15:33 +0900

    Update adder_tb.v

--- adder_tb.v
+++ adder_tb.v
@@ -5,6 +5,8 @@ module adder_tb;
     reg a, b, cin;
     wire sum, cout;
     integer file, scan_result;
+    parameter input_file ="input_vectors.txt";
+
 
     // Full Adder 인스턴스 생성
     adder uut (
@@ -16,21 +18,25 @@ module adder_tb;
     );
 
     initial begin
+
+        $dumpfile("test_out2.vcd");  // VCD 파일 생성
+        $dumpvars;  // 전체 신호 저장
         // 입력 벡터 파일 열기
-        file = $fopen("input_vectors.txt", "r");
+        file = $fopen(input_file, "r");
         if (file == 0) begin
-            $display("❌ Error: input_vectors.txt 파일을 찾을 수 없습니다.");
+            $display(" Error: not exist %s file", input_file);
             $finish;
         end
         
-        $display("✅ input_vectors.txt에서 입력 벡터를 읽어옵니다.");
+        $display("\n");
+        $display("read %s data ...\n", input_file);
         $display(" a | b | cin | sum | cout ");
         $display("-------------------------");
 
         // 파일에서 한 줄씩 읽어서 테스트 실행
         while (!$feof(file)) begin
             scan_result = $fscanf(file, "%d %d %d\n", a, b, cin);
-            #10;  // 10ns 대기 (Full Adder 연산 실행)
+            #1;  // 1ns 대기 (Full Adder 연산 실행)
             $display(" %b | %b |  %b  |  %b  |  %b  ", a, b, cin, sum, cout);
         end
 
