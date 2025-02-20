e7716ab284e6bba9dbe7ca66dd85fdadde1608fb
goeun-oh <103434703+goeun-oh@users.noreply.github.com>
2025-02-20 20:13:18 +0900

    컴파일 시 input.txt 파일을 참조하고, 결과물을 output.txt 파일에 로드하도록 수정

--- adder_tb.v
+++ adder_tb.v
@@ -1,48 +1,34 @@
-`timescale 1ns / 1ps
-
 module adder_tb;
-
     reg a, b, cin;
     wire sum, cout;
-    integer file, scan_result;
-    parameter input_file ="input_vectors.txt";
-
-
-    // Full Adder 인스턴스 생성
+    
     adder uut (
-        .a(a), 
-        .b(b), 
-        .cin(cin), 
-        .sum(sum), 
-        .cout(cout)
+        .a(a), .b(b), .cin(cin), .sum(sum), .cout(cout)
     );
 
-    initial begin
+    integer fd_in, fd_out;
+    integer scan_result;
 
-        $dumpfile("test_out2.vcd");  // VCD 파일 생성
-        $dumpvars;  // 전체 신호 저장
-        // 입력 벡터 파일 열기
-        file = $fopen(input_file, "r");
-        if (file == 0) begin
-            $display(" Error: not exist %s file", input_file);
+    initial begin
+        fd_in = $fopen("input.txt", "r"); // 입력 파일 열기
+        fd_out = $fopen("output.txt", "w"); // 출력 파일 열기
+
+        if (fd_in == 0) begin
+            $display("Error: input.txt file not found.");
             $finish;
         end
-        
-        $display("\n");
-        $display("read %s data ...\n", input_file);
-        $display(" a | b | cin | sum | cout ");
-        $display("-------------------------");
 
-        // 파일에서 한 줄씩 읽어서 테스트 실행
-        while (!$feof(file)) begin
-            scan_result = $fscanf(file, "%d %d %d\n", a, b, cin);
-            #1;  // 1ns 대기 (Full Adder 연산 실행)
-            $display(" %b | %b |  %b  |  %b  |  %b  ", a, b, cin, sum, cout);
+        $fwrite(fd_out, "A B Cin | Sum Cout\n"); // 결과 헤더 작성
+
+        while (!$feof(fd_in)) begin
+            scan_result = $fscanf(fd_in, "%b %b %b\n", a, b, cin); // input.txt에서 데이터 읽기
+            #10; // 연산 대기
+
+            $fwrite(fd_out, "%b %b %b | %b %b\n", a, b, cin, sum, cout); // 결과 저장
         end
 
-        // 파일 닫기
-        $fclose(file);
+        $fclose(fd_in);
+        $fclose(fd_out);
         $finish;
     end
-
 endmodule
