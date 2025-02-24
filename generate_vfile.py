3378f558fb1188dca4268dfc5e255493c0fa011a
jhyang1203 <xhvldk3@gmail.com>
2025-02-24 11:30:17 +0900

    Update generate_vfile.py

--- generate_vfile.py
+++ generate_vfile.py
@@ -1,7 +1,7 @@
 def gen_v(v, bit_width):
     with open(v, "w") as v:
-        v.write(f"module adder_n (input [{bit_width}:0] a, input [{bit_width}:0] b, input cin, output [{bit_width}:0] sum, output cout);\n")
-        v.write(f"    wire [{bit_width}:0] carry;\n\n")
+        v.write(f"module adder_n (input [{bit_width-1}:0] a, input [{bit_width-1}:0] b, input cin, output [{bit_width-1}:0] sum, output cout);\n")
+        v.write(f"    wire [{bit_width-1}:0] carry;\n\n")
         v.write(f"    adder FA0 (a[0], b[0], cin, sum[0], carry[0]);\n")
         for i in range(bit_width-2):
             v.write(f"    adder FA{i+1} (a[{i+1}], b[{i+1}], carry[{i}], sum[{i+1}], carry[{i+1}]);\n")
