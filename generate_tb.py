abfbdced85828ee8cc2656890e084998eb74f0b0
jhyang1203 <xhvldk3@gmail.com>
2025-02-24 11:17:33 +0900

    Update generate_tb.py

--- generate_tb.py
+++ generate_tb.py
@@ -1,7 +1,7 @@
 def gen_tb(tb, bit_width):
     instance_lines = ["adder_n uut (\n", "    .a(a), .b(b), .cin(cin), .sum(sum), .cout(cout)\n", " );\n\n" ]
     file_lines=["    integer fd_in, fd_out;\n", "    integer scan_result;\n", "\n"]
-    initial_lines=["    initial begin\n\n", '        fd_in = $fopen("input.txt", "r");\n','        fd_out = $fopen("verilog_output.txt", "w")\n\n', ]
+    initial_lines=["    initial begin\n\n", '        fd_in = $fopen("input.txt", "r");\n','        fd_out = $fopen("verilog_output.txt", "w");\n\n', ]
     is_fdin=["        if (fd_in == 0) begin\n", '            $display("Error: input.txt file not found.");\n', "            $finish;\n", "        end\n\n"]
     is_input_end=["        while (!$feof(fd_in)) begin\n", '            scan_result = $fscanf(fd_in, "%b %b %b\\n", a, b, cin);\n', "            #10;\n\n", '            $fwrite(fd_out, "%b %b %b -> sum: %b, cout: %b\\n", a, b, cin, sum, cout);\n', '        end\n\n']
     file_close=["        $fclose(fd_in);\n", "        $fclose(fd_out);\n", "        $finish;\n"]
