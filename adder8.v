a93ac1b69a39466be16bd80e00ad82d5c6520992
Yoo Jinmo <ujinmo0342@naver.com>
2025-02-24 10:52:37 +0900

    N비트 Full Adder

--- adder8.v
+++ adder8.v
@@ -1,9 +1,42 @@
-module adder8 (input [3:0] a, input [3:0] b, input cin, output [3:0] sum, output cout);
-    wire [3:0] carry;
-    
-    adder FA0 (a[0], b[0], cin, sum[0], carry[0]);
-    adder FA1 (a[1], b[1], carry[0], sum[1], carry[1]);
-    adder FA2 (a[2], b[2], carry[1], sum[2], carry[2]);
-    adder FA3 (a[3], b[3], carry[2], sum[3], cout);
+module adderN #(parameter WIDTH = 8) (
+    input  [WIDTH-1:0] a,
+    input  [WIDTH-1:0] b,
+    input              cin,
+    output [WIDTH-1:0] sum,
+    output             cout
+);
+    // 내부 carry 연결
+    wire [WIDTH-1:0] carry;
 
+    // 첫 번째 비트
+    adder FA0 (
+        .a(a[0]),
+        .b(b[0]),
+        .cin(cin),
+        .sum(sum[0]),
+        .cout(carry[0])
+    );
+
+    // 2번째 비트부터 WIDTH-2번째 비트까지
+    genvar i;
+    generate
+        for (i = 1; i < WIDTH-1; i = i + 1) begin : GEN_FA
+            adder FA_i (
+                .a(a[i]),
+                .b(b[i]),
+                .cin(carry[i-1]),
+                .sum(sum[i]),
+                .cout(carry[i])
+            );
+        end
+    endgenerate
+
+    // 마지막 비트
+    adder FA_last (
+        .a(a[WIDTH-1]),
+        .b(b[WIDTH-1]),
+        .cin(carry[WIDTH-2]),
+        .sum(sum[WIDTH-1]),
+        .cout(cout)
+    );
 endmodule
